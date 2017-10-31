ac748ea984dd7c1c23959013aed291092415c3fc
Hollis Medley <hmedley00@gmail.com>
2017-10-31 09:08:50 -0500

    made a tacobell.cs file and made changes to taco parser

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using log4net;
 using System.IO;
+using Geolocation;
 
 namespace LoggingKata
 {
@@ -16,16 +17,28 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
+            var path = Environment.CurrentDirectory + "//Taco_Bell-US-AL-Alabama.csv";
             if (args.Length == 0)
             {
                 Console.WriteLine("You must provide a filename as an argument");
-                Logger.Fatal("Cannot import without filename specified as an argument");
+                Logger.Fatal("Cannot import without filename specified in our path variable");
                 return;
             }
 
             Logger.Info("Log initialized");
+            Logger.Info("Grabbing from path" + path);
             var lines = File.ReadAllLines(args[0]);
+            if (lines.Length == 0)
+            {
+                Logger.Error("No locations to check for");
+            }
+            else if (lines.Length == 1)
+            {
+                Logger.Warn("Only one location provided");
+            }
             var parser = new TacoParser();
+            Logger.Debug("Initialized our Parser");
+
             var locations = lines.Select(line => parser.Parse(line));
 
             //TODO:  Find the two TacoBells in Alabama that are the furthurest from one another.
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
 using log4net;
 
 namespace LoggingKata
@@ -19,8 +23,40 @@ namespace LoggingKata
 
         public ITrackable Parse(string line)
         {
-            //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+            var listOfTacoBell = line.Split(',');
+            if (listOfTacoBell.Length <= 3)
+            {
+                Logger.Error("Must have at least three items in the array");
+                return null;
+            }
+            double lon = 0;
+            double lat = 0;
+
+            try
+            {
+                Logger.Debug("Attempt parsing longitude");
+                lon = double.Parse(listOfTacoBell[0]);
+                Logger.Debug("Attempt parsing latitude");
+                lat = double.Parse(listOfTacoBell[1]);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Failed to parse the location", e);
+                Console.WriteLine(e);
+                throw;
+            }
+
+            var tB = new TacoBell
+            {
+                Name = listOfTacoBell[2],
+                Location = new Point
+                {
+                    Longitude = lon,
+                    Latitude = lat
+                }
+            };
+
+            return tB;
         }
     }
 }
